c7766ede3a1e420b45338a8c7ee4a460987946ad
Forrest Waters <forrestwaters@gmail.com>
2020-01-02 18:01:38 +0800

    hot fix

--- src/Components/Organizer/OrganizerDemo.js
+++ src/Components/Organizer/OrganizerDemo.js
@@ -14,7 +14,7 @@ import { Link } from "react-router-dom";
 const initialEvents = [
   {
     email: "demo@demo.com",
-    name: "Demo Tourney",
+    eventName: "Demo Tourney",
     slug: "demotourney",
     teams: [
       "Big Dumps",
